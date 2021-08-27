7aee340c0176d2c8d566af937040d4be0cbf0cec
Dev-El-badry <dev.elbadry@gmail.com>
2021-08-27 06:23:11 +0200

    update statup message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 const port = 3000;
 
 const start = async () => {
-  console.log('startup message !!');
+  console.log('startup message second !!');
 
   if (!process.env.JWT_KEY) {
     throw new Error('must jwt key not be empty');
